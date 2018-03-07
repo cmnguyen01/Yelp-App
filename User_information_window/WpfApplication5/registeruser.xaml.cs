45d795407cdf32b5fc8b691c4c00b6d0cd3921d7
cnguyen <chris.m.nguyen@wsu.edu>
2018-03-07 12:32:30 -0800

    user_window before populating with data

---
+++ User_information_window/WpfApplication5/registeruser.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication5
+{
+    /// <summary>
+    /// Interaction logic for registeruser.xaml
+    /// </summary>
+    public partial class registeruser : Window
+    {
+        public registeruser()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void dataGrid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
